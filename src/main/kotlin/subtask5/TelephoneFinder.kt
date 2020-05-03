f1abc8b061e71fd1551853d596b9eaf75358232c
dev-ng <nickolay.golomysov@gmail.com>
2020-05-03 10:17:17 +0200

    Task5 done

--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,34 @@
 package subtask5
 
-import kotlin.reflect.KClass
-
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        val keyboard = mapOf(
+            '0' to arrayOf("8"),
+            '1' to arrayOf("2","4"),
+            '2' to arrayOf("1","3","5"),
+            '3' to arrayOf("2","6"),
+            '4' to arrayOf("1","5","7"),
+            '5' to arrayOf("2","4","6","8"),
+            '6' to arrayOf("3","5","9"),
+            '7' to arrayOf("4","8"),
+            '8' to arrayOf("5","7","9","0"),
+            '9' to arrayOf("6","8")
+        )
+
+        val len = number.length
+        var result: Array<String> = arrayOf()
+        for(i in 0..(len-1)) {
+            val toReplace = number[i]
+            if(! keyboard.containsKey(toReplace)) {
+                return null
+            }
+            val left = number.substring(0, i)
+            val right = number.substring(i + 1, len)
+            for(v in keyboard[toReplace]!!) {
+                result = result + (left + v + right)
+            }
+        }
+        return result
     }
 }
